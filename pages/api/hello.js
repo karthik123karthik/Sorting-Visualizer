adababea3951ae175bda53702866467fee7fbc6e
karthik g.k <kar221694@gmail.com>
2022-10-07 03:52:02 -0400

    added

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+import * as fs from "fs";
+
+
+export default function handler(req, res) {    
+     fs.readFile('dataPlace/db.json',"utf-8",(err,data)=>{
+       res.status(200).json(JSON.parse(data));      
+     })
+  }
