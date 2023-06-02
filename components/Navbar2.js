b39dd5a2532ee26a670c904899bc85cf000900a9
karthik g k <kar221694@gmail.com>
2023-06-03 05:22:12 +0530

    added styling

---
+++ components/Navbar2.js
@@ -0,0 +1,54 @@
+const Navbartwo = ({setArray}) => {
+
+
+    function cleargrid(){
+        setArray(Array.from({ length: 18 }, () => Array(30).fill(0)))
+      }
+
+
+
+  return (
+    <nav className="bg-gray-800 p-2">
+      <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <div className="flex-shrink-0 flex items-center">
+          <h3 className="text-xl text-white font-bold">ALGORITHM VISUALIZER</h3>
+        </div>
+        <div className="hidden sm:block sm:ml-6">
+          <div className="flex space-x-4">
+            <a
+              href="#"
+              className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+              aria-current="page"
+            >
+              bubble sort
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              merge sort
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              quick sort
+            </a>
+
+            <a
+              href="#"
+              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            >
+              selection sort
+            </a>
+          <button onClick={cleargrid} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border mx-2 border-blue-500 hover:border-transparent rounded">Clear Grid</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbartwo;
--- pages/paths.js
+++ pages/paths.js
@@ -1,35 +1,79 @@
 import { useState } from 'react';
 import { delay } from '../components/utilities';
+import Navbartwo from '../components/Navbar2';
+
+
+
 
 
 function Paths() {
 
-  const [arr, setArray] = useState(Array.from({ length: 15 }, () => Array(20).fill(0)));
+  const [arr, setArray] = useState(Array.from({ length: 18 }, () => Array(30).fill(0)));
 
   function giveIndex(row, col){
-       callvisited();
+       dfs(row, col, arr);
   }
 
-  async function callvisited(){
+  async function dfs(row, col, visited){
+   
+    if(row >= 18 || row<0 || col>= 30 || col<0 || visited[row][col]!==0)return;
+
+    
+    if(row === 3 && col ===3 ){
+      let temp = visited.slice();
+      temp[row][col] = 2
+      await delay(100);
+      setArray([...temp])
+      return;
+    }
+    
+    let temp = visited.slice();
+    temp[row][col] = 1
+    await delay(10);
+    setArray([...temp])
+    await delay(10);
+    let value = false
+    await dfs(row+1, col, arr)
+    await dfs(row, col+1, arr)
+    await dfs(row-1, col, arr)
+    await dfs(row, col-1, arr)
+
+    temp = visited.slice();
+    temp[row][col] = 1
+    await delay(10);
+    setArray([...temp])
+    await delay(10);
+    return;
+  
+  }
+
+ 
+
+  async function callvisited(row, col){
       let visitedArray = arr.slice();
       let queue = [];
 
-      queue.push([6,6]);
-      visitedArray[6][6] = 1;
+      queue.push([row, col]);
+      visitedArray[row][col] = 2;
       while(queue.length > 0){          
         let n = queue.length;
         visitedArray = arr.slice()
         while(n--){
          const [x, y] = queue.shift();
 
-         //if(x== 3 && y == 3)return
+         if(x== 3 && y == 3){
+          visitedArray = arr.slice()
+          visitedArray[x][y] = 2;
+          setArray([...visitedArray]);
+          return;
+         }
 
          if(x-1 >=0  && visitedArray[x-1][y] == 0){
           queue.push([x-1, y]);
           visitedArray[x-1][y] = 1
          }
 
-         if(x+1 <15  && visitedArray[x+1][y] == 0){
+         if(x+1 <18  && visitedArray[x+1][y] == 0){
           queue.push([x+1, y]);
           visitedArray[x+1][y] = 1
          }
@@ -39,7 +83,7 @@ function Paths() {
           visitedArray[x][y-1] = 1
          }
 
-         if(y+1<20   && visitedArray[x][y+1] == 0){
+         if(y+1<30   && visitedArray[x][y+1] == 0){
           queue.push([x, y+1]);
           visitedArray[x][y+1] = 1
          }
@@ -50,14 +94,23 @@ function Paths() {
       await delay(100)
     }
   }
+
+  function getcolor(ele){
+    if(ele === 1)return "rgb(21 94 117)";
+    else if(ele === 0)return "white";
+    else return "rgb(8 51 68)";
+  }
    
 
   return (
-    <div className='flex flex-row h-[600px] justify-center items-center'>
-    <div className='flex flex-col gap-1 h-[80vh] w-[80vw]'>
-    {arr.map((row, i) => < div key={i} className=' flex flex-row itmes-center justify-around gap-1'>{row.map((ele, j) => <div key={j} className='w-[100px] items-center text-center rounded' style={{"background":`${!ele? "red":"green"}`}} onClick={() => giveIndex(i, j)}>{ele}</div>)}</div>)}
+    <>
+      <Navbartwo setArray={setArray}/>
+    <div className='flex flex-row justify-center items-center'>
+    <div className='flex flex-col mt-2 bg-cyan-400 gap-1 w-[80vw]'>
+    {arr.map((row, i) => < div key={i} className=' flex flex-row itmes-center justify-around gap-1'>{row.map((ele, j) => <div key={j} className='w-[100px] items-center text-center ' style={{"background":`${getcolor(ele)}`}} onClick={() => giveIndex(i, j)}>{ele}</div>)}</div>)}
     </div>
     </div>
+    </>
   )
 }
 
